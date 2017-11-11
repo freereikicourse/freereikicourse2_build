794588556728e25f20d8457338f7778b4326d2c4
Reiki <genericstorage2@gmail.com>
2017-11-11 23:24:28 +0000

    Updates

--- ml.js
+++
@@ -1,7 +0,0 @@
-function em_click(){
-location.href="mailto:support@freereikicourse.com";
-return true; }
-function ml_write(){
-document.write('support@freereikicourse.com');}
-function ml_hp_write(){
-document.write('<img title="support@freereikicourse.com"');}
